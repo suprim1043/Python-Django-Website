274bf56972389b9a9f32f409f3fce3887224e40d
Suprim Sedhai <Suprim1043@gmail.com>
2023-07-25 19:05:05 -0500

    new

--- MerakiWebsite/settings.py
+++ MerakiWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1e^u1wr*akzfv&6+h7_g*tz^vstzvs2u1-lr+9u)f8uiz-jwsc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
