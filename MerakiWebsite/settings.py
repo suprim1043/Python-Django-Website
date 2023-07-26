5ee9b5d706a187f6b10c523331a005ca1c267ff8
Suprim Sedhai <65957245+suprim1043@users.noreply.github.com>
2023-07-26 18:42:56 -0500

    Update settings.py

--- MerakiWebsite/settings.py
+++ MerakiWebsite/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',s
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
