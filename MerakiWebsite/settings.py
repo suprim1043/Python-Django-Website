02d03602398c352dcb09b03df7330fe3f236cc8a
Suprim Sedhai <65957245+suprim1043@users.noreply.github.com>
2023-07-26 19:01:18 -0500

    Update settings.py

--- MerakiWebsite/settings.py
+++ MerakiWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1e^u1wr*akzfv&6+h7_g*tz^vstzvs2u1-lr+9u)f8uiz-jwsc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://merakicosmetics.herokuapp.com','http://127.0.0.1:8000','*']
+ALLOWED_HOSTS = ['https://merakicosmetics.herokuapp.com','http://127.0.0.1:8000','https://merakiwebsite-e64bf7c1cbe5.herokuapp.com']
 
 
 # Application definition
