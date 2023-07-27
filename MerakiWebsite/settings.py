6c3b061a57743f22a2f102ea0ef9999695bd2ba3
Suprim Sedhai <Suprim1043@gmail.com>
2023-07-26 19:56:22 -0500

    J

--- MerakiWebsite/settings.py
+++ MerakiWebsite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-MEDIA_URL = 'statics/images/'
+MEDIA_URL = 'static/images/'
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'statics')
